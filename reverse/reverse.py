b53b85c3243c8fe70d7834bb53134c5cb00ec989
Corbin Robb <corbinrobb@gmail.com>
2020-07-17 10:17:53 -0600

    Meet MVP and attempt stretch

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,13 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node is None:
+            return
+
+        if not node.get_next():
+            self.head = node
+
+        if node.get_next():
+            self.reverse_list(node.get_next(), node)
+
+        node.set_next(prev)
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = []
+        self.capacity = capacity
+        self.oldest = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            self.storage[self.oldest] = item
+            if self.oldest + 1 == self.capacity:
+                self.oldest = 0
+            else:
+                self.oldest += 1
+        else:
+            self.storage.append(item)
+
 
     def get(self):
-        pass
+        return self.storage
